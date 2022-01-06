a974ce2e5667c2b92fd657653d31f5b8293b86ab
AuWiMo <yodacanfly@gmail.com>
2022-01-05 23:54:27 -0500

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,62 @@
+let gridSize = 16;
+let cubeSize = 14;
+let appleX, appleY;
+let gameOver;
+let started = false;
+
+function setup() {
+  createCanvas(430, 430);
+  background(0);
+  // for(i = 0; i <= 26; i++) {
+  //     rect(gridSize * i, gridSize * i, cubeSize);
+  // }
+  // for(i = 0; i <= 26; i++) {
+  //     rect(gridSize * i, (26 - i) * gridSize, cubeSize);
+  // }
+  snake = new Snake(gridSize * 13, gridSize * 20);
+  frameRate(8);
+  appleX = floor(random(0, 26)) * gridSize;
+  appleY = floor(random(0, 26)) * gridSize;
+  textAlign(CENTER);
+  textSize(24);
+  textStyle(NORMAL);
+  fill(255);
+  text("HIT ANY KEY TO BEGIN", gridSize * 13.5, gridSize * 14.5);
+}
+
+function draw() {
+  if(started) {
+    snake.eat(appleX, appleY);
+    if (!gameOver) {
+      background(0);
+      fill(255, 0, 0);
+      rect(appleX, appleY, cubeSize);
+      snake.update();
+      fill(0, 230, 0);
+      snake.show();
+    } else {
+      textSize(50);
+      textStyle(BOLD);
+      fill(75, 0, 0);
+      text("GAME OVER!", gridSize * 13.5, gridSize * 14.5);
+    }
+  }
+}
+
+function keyPressed() {
+  started = true;
+  if (keyCode === RIGHT_ARROW || keyCode === 68) {
+    snake.speedChange(1, 0);
+  }
+  if (keyCode === LEFT_ARROW || keyCode === 65) {
+    snake.speedChange(-1, 0);
+  }
+  if (keyCode === UP_ARROW || keyCode === 87) {
+    snake.speedChange(0, -1);
+  }
+  if (keyCode === DOWN_ARROW || keyCode === 83) {
+    snake.speedChange(0, 1);
+  }
+}
+
+
---
+++ snake.js
@@ -0,0 +1,64 @@
+function Snake(startX, startY) {
+  this.x = startX;
+  this.y = startY;
+  this.xSpeed = 0;
+  this.ySpeed = -1;
+  this.total = 0;
+  this.tail = [];
+  
+  this.show = function() {
+    for (let i = 0; i < this.tail.length; i++) {
+      rect(this.tail[i].x, this.tail[i].y, 14, 14);
+    }
+    rect(this.x, this.y, cubeSize, cubeSize)
+    textAlign(CENTER);
+    textSize(12);
+    textStyle(NORMAL);
+    fill(255);
+    text("Score: " + this.total, gridSize * 2, gridSize * 1.5);
+    
+  }
+  
+  this.eat = function(ax, ay) {
+    if (appleX == this.x && appleY == this.y) {
+      appleX = floor(random(0, 26)) * gridSize;
+      appleY = floor(random(0, 26)) * gridSize;
+      this.total++;
+    }
+  }
+  
+  this.update = function() {
+    if (this.total == this.tail.length) {
+      for (let i = 0; i < this.tail.length - 1; i++) {
+        this.tail[i] = this.tail[i+1];
+      }
+    }
+    if(this.total >= 1) {
+      this.tail[this.total - 1] = createVector(this.x, this.y);
+    }
+    this.x += gridSize * this.xSpeed;
+    this.y += gridSize * this.ySpeed;
+    if (this.x > width || this.x < 0 || this.y < 0 || this.y > height) {
+      this.death();
+    }
+    for (let i = 0; i < this.tail.length; i++) {
+      if (this.tail[i].x == this.x && this.tail[i].y == this.y) {
+        this.death();
+      }
+    }
+  }
+  
+  
+  this.speedChange = function(x, y) {
+    if(x != -this.xSpeed || y != -this.ySpeed) {
+      this.xSpeed = x;
+      this.ySpeed = y;
+    }
+  }
+  
+  this.death = function() {
+    gameOver = true;
+    
+  }
+
+}
